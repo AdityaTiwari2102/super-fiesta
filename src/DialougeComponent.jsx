de72c4115c09a51f8bb193a70b4c3f82f7594e79
ADITYA TIWARI <taditya.tiwari007@gmail.com>
2021-10-08 15:59:42 +0530

    Feat: Dialouge component

---
+++ src/DialougeComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export function Dialouge() {
+
+  return (
+    <div>
+      <p>
+        Want to here a dialouge???
+        <br/>
+        <button onClick={speakDialouge}>
+          Click Me
+        </button>
+      </p>
+    </div>
+  )
+}
